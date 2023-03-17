1ba70b49bf0ecec9e43a01486c76796e8c5cc774
Javier Urien <javierurien@spacesur.com>
2023-03-17 20:43:30 +0100

    fix for sh shell

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@ export COMPILER_FLAGS="-Wall -Wno-format-security -Wno-format-nonliteral -Wno-wr
 export BUILD_TYPE=Release
 export OUT_OF_SOURCE_BUILD=true
 
-if [[ "${OUT_OF_SOURCE_BUILD}" == "false" ]] ; then unset OUT_OF_SOURCE_BUILD ; fi
+if [ "${OUT_OF_SOURCE_BUILD}" = "false" ] ; then unset OUT_OF_SOURCE_BUILD ; fi
 
 . $HOME/.build_environment
 . $HOME/opt/local/share/sur/build_environment.sh
 
 export SKIP_ANALISIS=True
 export COMPILER_PREFIX=i686-w64-mingw32
+export MAKE_PARAMS="-j 1"
 
 /bin/sh lib/suri/tools/ci_build.sh
 
